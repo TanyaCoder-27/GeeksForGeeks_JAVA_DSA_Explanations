53d21b0bf1e83dfeb55a4e7aefdf8a08cefc7e90
ANDRA TULASI LAKSHMI TANYA <tanyakonapala@gmail.com>
2025-02-28 19:23:25 +0530

    Update Frog Jump.java

--- Dynamic Programming/Frog Jump.java
+++ Dynamic Programming/Frog Jump.java
@@ -53,3 +53,28 @@ class Solution {
         
     }
 }
+
+//Tabulation
+
+class Solution {
+    int minCost(int[] height) {
+        // code here
+        int n = height.length;
+        int prev =0, prev2 =0;
+        
+        for(int i=1;i<n;i++){
+            int jumpOne = prev + Math.abs(height[i]- height[i-1]);
+            int jumpTwo = Integer.MAX_VALUE;
+            if(i>1){
+                jumpTwo = prev2 + Math.abs(height[i] - height[i-2]);
+            }
+            
+            int curr = Math.min(jumpOne,jumpTwo);
+            prev2 = prev;
+            prev = curr;
+        }
+        
+        return prev;
+        
+    }
+}
